b8450c66d978d2131ad4977972861bc34f834113
Maxime CHAMPIGNEUX <contact@maximechampigneux.fr>
2026-09-11 09:55:27 +0200

    feat: authenticate collaborators

--- views/login_view.py
+++ views/login_view.py
@@ -7,3 +7,11 @@ def show_login_form():
     password = getpass("Mot de passe : ")
 
     return email, password
+
+
+def show_login_error(message):
+    print(f"Erreur : {message}")
+
+
+def show_login_success(collaborator):
+    print(f"Bienvenue {collaborator.first_name} {collaborator.last_name}")
